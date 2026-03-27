398bcc7e2b7c1170b3ee40fa77091ebd7a03ff2e
erick <e.velasquez.tech@gmail.com>
2026-03-27 19:03:03 +0100

    Templates commit 2

--- src/main/java/org/ies/tierno/template/BaseTemplate.java
+++ src/main/java/org/ies/tierno/template/BaseTemplate.java
@@ -2,6 +2,6 @@ package org.ies.tierno.template;
 
 public class BaseTemplate {
     public static void main(String[] args) {
-        System.out.println("Usalo con sabiduaria");
+        System.out.println("Usalo con sabiduria");
     }
 }
